693e77836ce4cf42091a81b2a9478694aa35cd3d
JC Gonzalez <devgonzalez25@gmail.com>
2019-09-30 04:58:11 -0500

    reording the files for better understanding of some database stuff ive implemented

--- check.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION student_add() RETURNS TRIGGER AS
-$$ 
-  BEGIN
-    if new.sid NOT IN (SELECT sid FROM credits) then
-      INSERT INTO credits VALUES (new.sid)
-  END;
-$$ psgsql
